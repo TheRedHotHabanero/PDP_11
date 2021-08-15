4273134181d1517d39f1beebffd6c707bb35ea76
theredhothabanero <lkrn10001@gmail.com>
2021-08-15 23:19:33 +0300

    started work

---
+++ pdp_11.cpp
@@ -0,0 +1,26 @@
+#include "pdp_11.h"
+
+void b_write(Adress adr, Byte byte)
+{
+
+}
+
+Byte b_read(Adress adr)
+{
+
+}
+
+void w_write(Adress adr, Word word)
+{
+
+}
+
+Word w_read(Adress adr)
+{
+  
+}
+
+int main()
+{
+
+}
