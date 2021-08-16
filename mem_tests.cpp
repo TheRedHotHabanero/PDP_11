583412c467f6efb5900730d3aef1b8ec1014a018
theredhothabanero <lkrn10001@gmail.com>
2021-08-16 18:04:04 +0300

    working memory tests and functions definition

---
+++ mem_tests.cpp
@@ -0,0 +1,33 @@
+#include "mem_tests.h"
+#include "pdp_11.h"
+
+void test_byte_read_write_1()
+{
+  Byte new_byte = 0x0a;
+  Adress new_adr = 2;
+
+  b_write(new_adr, new_byte);
+  Byte testing_byte = b_read(new_adr);
+
+  assert(new_byte == testing_byte);
+}
+
+void test_word_read_2()
+{
+  Adress new_adr = 4;
+  Byte first_byte = 0x0a;
+  Byte second_byte = 0x0b;
+  Word testing_word = 0x0b0a;
+
+  b_write(new_adr, first_byte);
+  b_write(new_adr + 1, second_byte);
+  Word result_word = w_read(new_adr);
+
+  assert(result_word == testing_word);
+}
+
+void mem_test()
+{
+  test_byte_read_write_1();
+  test_word_read_2();
+}
---
+++ mem_tests.h
@@ -0,0 +1,11 @@
+#ifndef PDP_11_MEM_TESTS_H
+#define PDP_11_MEM_TESTS_H
+
+#include <cassert>
+#include <iostream>
+
+void test_byte_read_write_1();
+void test_word_read_2();
+void mem_test();
+
+#endif //PDP_11_MEM_TESTS_H
